817cd2e91406cc59274bc3af892157ece985cda2
Alex Eagle <alexeagle@google.com>
2017-10-07 12:28:48 -0700

    Move concat inside the express server and fix sourcemaps

--- devserver.js
+++ devserver.js
@@ -1,39 +1,51 @@
 const express = require('express')
 const livereload = require('livereload')
-const {createInterface} = require('readline');
+const fs = require('fs')
+const {createInterface} = require('readline')
 const app = express()
 
 // fixme: using FAILURE here to match current ibazel
 // should be SUCCEEDED
 const IBAZEL_NOTIFY_BUILD_SUCCESS = 'IBAZEL_BUILD_COMPLETED FAILURE';
 
-function ibazel_listen(callback) {
-  const rl = createInterface({input: process.stdin, terminal: false});
-  rl.on('line', chunk => {
-    if (chunk === IBAZEL_NOTIFY_BUILD_SUCCESS) {
-      var start = Date.now();
-      callback();
-    }
-  });
-  rl.on('close', () => {
-    // Give ibazel 5s to kill our process, otherwise do it ourselves
-    setTimeout(() => {
-      console.error('ibazel failed to stop karma; probably a bug');
-      process.exit(1);
-    }, 5000);
-  });
-}
-
-// listen on stdin
-// -> "watch mode for express"
-// -> livereload
+const lrserver = livereload.createServer({debug: false})
+const rl = createInterface({input: process.stdin, terminal: false});
+rl.on('line', chunk => {
+  if (chunk === IBAZEL_NOTIFY_BUILD_SUCCESS) {
+    lrserver.refresh('./bundle.js')
+  }
+});
+rl.on('close', () => {
+  // Give ibazel 5s to kill our process, otherwise do it ourselves
+  setTimeout(() => {
+    console.error('ibazel failed to stop devserver; probably a bug');
+    process.exit(1);
+  }, 5000);
+});
 
 app.use(express.static('.'))
 
-const lrserver = livereload.createServer({debug: false})
-ibazel_listen(() => {
-  lrserver.refresh('./bundle.js')
-});
+app.get('/bundle.js', function (req, res) {
+  const start = new Date();
+  var lineReader = createInterface({
+    input: fs.createReadStream('devsources.MF')
+  });
+  
+  lineReader.on('line', function (file) {
+    // look up one directory, because the manifest contains workspace names
+    let content = fs.readFileSync(`../${file}`, {encoding: 'utf-8'})
+        // Remove 'use strict'.
+        .replace(/('use strict'|"use strict");?/, '');
+    content = JSON.stringify(`${content}\n//# sourceURL=http://concatjs/base/${file}\n`);
+    content = `//${file}\neval(${content});\n`;  
+    res.write(content, {encoding: 'utf-8'})
+  });
+  lineReader.on('close', () => {
+    console.log(`bundled JS in ${new Date() - start}ms`)
+    res.end()
+  });
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
